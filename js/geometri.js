086f795a5d7baa99b8358a56849fc552b17829a0
shahin2525 <shahin451999@gmail.com>
2023-03-03 16:56:12 -0800

    Rhombus

--- js/geometri.js
+++ js/geometri.js
@@ -2,9 +2,10 @@
 document.getElementById("blog-button").addEventListener("click", function () {
   window.location.href = "blog.html";
 });
-
+let serial = 0;
 // 1 measurement manupulate triangle
 document.getElementById("triangle").addEventListener("click", function () {
+  serial = serial + 1;
   // step 1
   const triangleText = document.getElementById("triangle-text").innerText;
   // step 2
@@ -22,7 +23,7 @@ document.getElementById("triangle").addEventListener("click", function () {
   // step 5
   const triangleAreaMultiple =
     triangleInputBase * triangleInputHight * triangleNumber;
-  const triangleArea = triangleAreaMultiple.toFixed(2);
+  const triangleArea = triangleAreaMultiple.toFixed(2) + "cm";
 
   // console.log(
   //   triangleText,
@@ -30,19 +31,24 @@ document.getElementById("triangle").addEventListener("click", function () {
   //   triangleInputBase,
   //   triangleInputHight
   // );
+  displayData(triangleText, triangleArea);
+});
+// function 1
+function displayData(triangleText, triangleArea) {
   const container = document.getElementById("table-container");
   const tr = document.createElement("tr");
   tr.innerHTML = `
-  <td>${1}</td>
+  <td>${serial}</td>
   <td>${triangleText}</td> 
   <td>${triangleArea}</td>
  `;
 
   container.appendChild(tr);
-});
+}
 
 // 2 measurement manupulate Rectangular
 document.getElementById("rectangle-btn").addEventListener("click", function () {
+  serial += 1;
   const rectangleText = document.getElementById("rectangle-text").innerText;
   const rectangleInputWidthString =
     document.getElementById("rectangle-w").value;
@@ -50,23 +56,25 @@ document.getElementById("rectangle-btn").addEventListener("click", function () {
   const rectangleInputLengthString =
     document.getElementById("rectangle-l").value;
   const rectangleInputLength = parseFloat(rectangleInputLengthString);
-  const rectangleArea = rectangleInputWidth * rectangleInputLength;
-
+  const rectangleAreaMultiple = rectangleInputWidth * rectangleInputLength;
+  const rectangleArea = rectangleAreaMultiple.toFixed(2) + " cm";
+  displayData(rectangleText, rectangleArea);
   // console.log(rectangleText, rectangleInputWidth, rectangleInputLength);
 
-  const container = document.getElementById("table-container");
-  const tr = document.createElement("tr");
-  tr.innerHTML = `
-<td>${2}</td>
- <td>${rectangleText}</td>
- <td>${rectangleArea}</td>`;
-  container.appendChild(tr);
+  //   const container = document.getElementById("table-container");
+  //   const tr = document.createElement("tr");
+  //   tr.innerHTML = `
+  // <td>${2}</td>
+  //  <td>${rectangleText}</td>
+  //  <td>${rectangleArea}</td>`;
+  //   container.appendChild(tr);
 });
 
 // 3 measurement manupulate Parallelogram
 document
   .getElementById("Parallelogram-btn")
   .addEventListener("click", function () {
+    serial += 1;
     //  step 1
     const parallelogramText =
       document.getElementById("Parallelogram-text").innerText;
@@ -79,13 +87,14 @@ document
       document.getElementById("Parallelogram-h").innerText;
     const parallelogramHight = parseFloat(parallelogramHightString);
     // step 4
-    const parallelogramArea = parallelogramBase * parallelogramHight;
-
-    const container = document.getElementById("table-container");
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-    <td>${3}</td>
-    <td>${parallelogramText}</td>
-    <td>${parallelogramArea}</td>`;
-    container.appendChild(tr);
+    const parallelogramAreaMultiple = parallelogramBase * parallelogramHight;
+    const parallelogramArea = parallelogramAreaMultiple.toFixed(2) + "cm";
+    displayData(parallelogramText, parallelogramArea);
+    // const container = document.getElementById("table-container");
+    // const tr = document.createElement("tr");
+    // tr.innerHTML = `
+    // <td>${3}</td>
+    // <td>${parallelogramText}</td>
+    // <td>${parallelogramArea}</td>`;
+    // container.appendChild(tr);
   });
